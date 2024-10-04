845a8aaaad0064c8d6d73c31a9ff0c100c74f149
guillaumemichel <guillaume@michel.id>
2024-10-04 10:08:56 +0200

    removing agent and maddrs from antlog

--- antslog/logs.go
+++ antslog/logs.go
@@ -4,7 +4,6 @@ import (
 	"time"
 
 	"github.com/libp2p/go-libp2p/core/peer"
-	ma "github.com/multiformats/go-multiaddr"
 	mh "github.com/multiformats/go-multihash"
 )
 
@@ -14,6 +13,4 @@ type RequestLog struct {
 	Requester peer.ID
 	Type      uint8
 	Target    mh.Multihash
-	Maddrs    []ma.Multiaddr
-	Agent     string
 }
